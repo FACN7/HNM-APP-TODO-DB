9f3f1f75286018fa1b713c268ae9c692fd0afeab
nataphilips <fnd.natalia@gmail.com>
2019-10-30 17:52:14 +0200

    Display users tasks Relates #9

--- src/queries/task_list_byUser.js
+++ src/queries/task_list_byUser.js
@@ -1,15 +1,15 @@
 const dbConnection = require("../database/db_connection");
 
 //show the list of tasks that belong to specific user who take the task
-const task_list_byUser = (user_id, cb) => {
-    dbConnection.query(
-        ` select content from tasks where user_id=${user_id} and task_done =false and task_taken = true `,
-        (err, res) => {
-            if (err) return cb(err);
-            console.log("res.rows=:", res.rows);
-            cb(null, res.rows);
-        }
-    );
-}
+const task_list_byUser = cb => {
+  dbConnection.query(
+    ` select * from tasks where task_done =false and task_taken = true `,
+    (err, res) => {
+      if (err) return cb(err);
+      console.log("res.rows=:", res.rows);
+      cb(null, res.rows);
+    }
+  );
+};
 
 module.exports = task_list_byUser;
